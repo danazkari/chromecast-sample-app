156effd0424fe9f5db6f5006513a3d00f9c7a4ba
Daniel Prado <danazkari@gmail.com>
2017-06-05 23:34:40 -0600

    removed unnecessary console.log and variable

--- public/sender-app.js
+++ public/sender-app.js
@@ -3,10 +3,8 @@
 const APP_ID = 'A1042A86';
 
 window['__onGCastApiAvailable'] = function __onGCastApiAvailable(isAvailable) {
-    console.log(isAvailable);
     cast.framework.CastContext.getInstance()
         .setOptions({
             receiverApplicationId: APP_ID
         });
-    const castSession = cast.framework.CastContext.getInstance().getCurrentSession();
 };
